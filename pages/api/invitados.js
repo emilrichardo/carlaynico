fea472bee9627112080656eb3b0961f245464280
emilrichardo <emilrichardo@gmail.com>
2022-03-28 13:58:40 -0300

    api invitados

---
+++ pages/api/invitados.js
@@ -0,0 +1,278 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json(
+    [
+      {
+       "id": 1,
+       "nombres": "Anglade Manuel",
+       "personal": 1
+      },
+      {
+       "id": 2,
+       "nombres": "Cecilia - Daniel y Lorenzo ",
+       "personal": 2
+      },
+      {
+       "id": 3,
+       "nombres": "Susana y Fransico",
+       "personal": 2
+      },
+      {
+       "id": 4,
+       "nombres": "Rodrigo Fernandez",
+       "personal": 1
+      },
+      {
+       "id": 5,
+       "nombres": "Anahi Maximo y Agustina",
+       "personal": 3
+      },
+      {
+       "id": 6,
+       "nombres": "Enrique y Liliana",
+       "personal": 2
+      },
+      {
+       "id": 7,
+       "nombres": "Gonzalo y Romina",
+       "personal": 2
+      },
+      {
+       "id": 8,
+       "nombres": "Luciano y Meli",
+       "personal": 2
+      },
+      {
+       "id": 9,
+       "nombres": "Matias Distacio",
+       "personal": 1
+      },
+      {
+       "id": 10,
+       "nombres": "Gabriel y Milagros",
+       "personal": 2
+      },
+      {
+       "id": 11,
+       "nombres": "Ignacio y Yanina",
+       "personal": 2
+      },
+      {
+       "id": 12,
+       "nombres": "Emil y Romi",
+       "personal": 2
+      },
+      {
+       "id": 13,
+       "nombres": "Ramiro y Laura ",
+       "personal": 2
+      },
+      {
+       "id": 14,
+       "nombres": "Diego y Nadia",
+       "personal": 2
+      },
+      {
+       "id": 15,
+       "nombres": "Pablo y Silvia ",
+       "personal": 2
+      },
+      {
+       "id": 16,
+       "nombres": "Rodrigo y Noelia ",
+       "personal": 2
+      },
+      {
+       "id": 17,
+       "nombres": "Jose y Carolina ",
+       "personal": 2
+      },
+      {
+       "id": 18,
+       "nombres": "Mario y Mariela ",
+       "personal": 2
+      },
+      {
+       "id": 19,
+       "nombres": "Dante y Silvia",
+       "personal": 2
+      },
+      {
+       "id": 20,
+       "nombres": "Hugo y Magdalena ",
+       "personal": 2
+      },
+      {
+       "id": 21,
+       "nombres": "Hector y Katherine ",
+       "personal": 2
+      },
+      {
+       "id": 22,
+       "nombres": "Marcos y Carla ",
+       "personal": 2
+      },
+      {
+       "id": 23,
+       "nombres": "Feliciana Mariela y Jose ",
+       "personal": 3
+      },
+      {
+       "id": 24,
+       "nombres": "Ivan - Veronica y Sophia",
+       "personal": 2
+      },
+      {
+       "id": 25,
+       "nombres": "Eward - Lucia - Diego y Justina ",
+       "personal": 3
+      },
+      {
+       "id": 26,
+       "nombres": "Carlos - Sandra - Carlos Hijo - Sandra Hija y Mariana ",
+       "personal": 5
+      },
+      {
+       "id": 27,
+       "nombres": "Rita - Ale - Nicolas y Lara ",
+       "personal": 4
+      },
+      {
+       "id": 28,
+       "nombres": "Luz y Emanuel",
+       "personal": 2
+      },
+      {
+       "id": 29,
+       "nombres": "Pascual Suarez",
+       "personal": 1
+      },
+      {
+       "id": 30,
+       "nombres": "Mariano y Lara",
+       "personal": 2
+      },
+      {
+       "id": 31,
+       "nombres": "Luciano y Carla ",
+       "personal": 2
+      },
+      {
+       "id": 32,
+       "nombres": "Luciana Abatedaga y Gladys Fornero",
+       "personal": 2
+      },
+      {
+       "id": 33,
+       "nombres": "Dabata y German",
+       "personal": 2
+      },
+      {
+       "id": 34,
+       "nombres": "Maia y Bruno ",
+       "personal": 2
+      },
+      {
+       "id": 35,
+       "nombres": "Gabriela Garcia",
+       "personal": 1
+      },
+      {
+       "id": 36,
+       "nombres": "Tamara Coronel",
+       "personal": 1
+      },
+      {
+       "id": 37,
+       "nombres": "Antonella y Rodo ",
+       "personal": 2
+      },
+      {
+       "id": 38,
+       "nombres": "Mayra Diaz",
+       "personal": 1
+      },
+      {
+       "id": 39,
+       "nombres": "Esther y Milagros",
+       "personal": 2
+      },
+      {
+       "id": 40,
+       "nombres": "Natalia Luna",
+       "personal": 1
+      },
+      {
+       "id": 41,
+       "nombres": "Fatima ",
+       "personal": 1
+      },
+      {
+       "id": 42,
+       "nombres": "Tania Vera ",
+       "personal": 1
+      },
+      {
+       "id": 43,
+       "nombres": "Rosana y Cesar",
+       "personal": 2
+      },
+      {
+       "id": 44,
+       "nombres": "Mario y Patricia",
+       "personal": 2
+      },
+      {
+       "id": 45,
+       "nombres": "Cristian y Gabriela",
+       "personal": 2
+      },
+      {
+       "id": 46,
+       "nombres": "Ana Gabriela Coroleu",
+       "personal": 1
+      },
+      {
+       "id": 47,
+       "nombres": "Jose Aroldo Leguizamon",
+       "personal": 1
+      },
+      {
+       "id": 48,
+       "nombres": "Natalia Martin",
+       "personal": 1
+      },
+      {
+       "id": 49,
+       "nombres": "Tiara ",
+       "personal": 1
+      },
+      {
+       "id": 50,
+       "nombres": "Romina y jorge",
+       "personal": 2
+      },
+      {
+       "id": 51,
+       "nombres": "Maximiliano y Eugenia ",
+       "personal": 2
+      },
+      {
+       "id": 52,
+       "nombres": "Rita y Dario",
+       "personal": 2
+      },
+      {
+       "id": 53,
+       "nombres": "Santiago Coroleu y Silvina Barcudi",
+       "personal": 2
+      },
+      {
+       "id": null,
+       "nombres": "total",
+       "personal": 100
+      }
+     ]
+  )
+}
