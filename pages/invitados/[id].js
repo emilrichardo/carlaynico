d3f2768dab8ff69db27a628e753ba146f1337dc6
emilrichardo <emilrichardo@gmail.com>
2022-03-28 17:29:52 -0300

    id page

--- pages/invitados/[id].js
+++ pages/invitados/[id].js
@@ -1,7 +1,7 @@
-/* import Invitaction from "../../components/Invitation";
+import Invitaction from "../../components/Invitation";
 
 export const getStaticPaths = async () => {
-    const res = await fetch('http://localhost:3000/api/invitados');
+    const res = await fetch('https://carlaynico.vercel.app/api/invitados');
     const data = await res.json();
 
     // map data to an array of path objects with params (id)
@@ -19,17 +19,17 @@ export const getStaticPaths = async () => {
 
   export const getStaticProps = async (context) => {
     const id = context.params.id;
-    const res = await fetch('http://localhost:3000/api/invitados');
+    const res = await fetch('https://carlaynico.vercel.app/api/invitados');
     const data = await res.json();
 
     return {
       props: { invitado: data[id] }
     }
-  } */
+  }
 
   const Details = ({ invitado }) => {
     return (
-     <>invitados</>
+      <Invitaction nombres={invitado.nombres} personal={invitado.personal} />
     );
   }
 
